12fb3c29349f34d70f16aa937457d851a4fe341a
wang <englishwlw@126.com>
2020-09-18 10:48:15 +0800

    Initial commit

---
+++ src/test/java/com/springboot/mmy/MmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
